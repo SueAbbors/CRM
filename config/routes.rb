0f0b74ee6cf75e52e3319fdfaa71ad9d6c551d33
Sue M. MacDonald <smacdonald@hydro.mb.ca>
2015-04-06 23:24:14 -0500

    added missing email, alpha and customers.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   resources :customers
   
   root to: 'finder#index'
+  
+  get 'alphabetized'   =>  'finder#alphabetized,   as: 'alphabetized'
+  get 'missing_email' =>  'finder#missing_email', as: 'missing_email'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
